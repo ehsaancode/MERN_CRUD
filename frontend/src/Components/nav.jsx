46bd3db3ad94ee3196dc207a042fec5caf8a1c19
Ehsaan Mondal <mail.ehsaan@gmail.com>
2024-07-10 16:47:59 +0530

    Update nav.jsx

--- frontend/src/Components/nav.jsx
+++ frontend/src/Components/nav.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const nav = () => {
+const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <div className="navbar-brand">MERN</div>
-   
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to= "/create" className="nav-link active" aria-current="page">Create</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/" className="nav-link">All posts</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-  )
+      <div className="container-fluid">
+        <div className="navbar-brand">MERN</div>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/create" className="nav-link active" aria-current="page">Create</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">All posts</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default nav
+export default Nav;
